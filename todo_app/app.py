98d4d8cd54de5c635359837d7ccf6da20039127a
banao <tatitachi870@gmail.com>
2022-07-22 20:50:27 +0900

    modify env

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,8 @@
 from flask_todo import app
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=os.getenv("PORT") , debug=True)
